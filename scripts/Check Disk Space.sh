301902799d6a3ae2573f792426c2f636d507a596
grvkhanna <grvkhanna@fcfd737a-d280-4630-ab2b-24b74127339d>
2016-03-29 13:52:36 +0000

    Updates

---
+++ scripts/Check Disk Space.sh
@@ -0,0 +1,12 @@
+Check Disk Space
+
+#!/bin/bash
+MAX=5
+PART=/dev/xvda1
+USE=`df -h |grep $PART | awk '{ print $5 }' | cut -d'%' -f1`
+if [ $USE -gt $MAX ]; then
+  echo "Percent used: $USE" 
+  echo "Running out of disk space"
+fi
+
+Reference : http://www.tecmint.com/basic-shell-programming-part-ii/
